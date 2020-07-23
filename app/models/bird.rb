8f60704e06c9ad399a30d5ec2f0b6c62546f7478
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-23 19:50:21 +0000

    Done.

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,19 @@
 class Bird < ApplicationRecord
 end
+
+Rails.application.routes.draw do
+  get '/birds' => 'birds#index'
+  get '/birds/:id' => 'birds#show' # new
+end
+
+class BirdsController < ApplicationController
+  def index
+    birds = Bird.all
+    render json: birds
+  end
+ 
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+  end
+end
